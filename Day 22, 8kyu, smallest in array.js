f0d1b4e0c16c4056ddbb6f999b519c0ebeff3a5f
Irina <irina@theluky.com>
2022-04-09 20:57:03 -0400

    Add Day 22 8kyu

---
+++ Day 22, 8kyu, smallest in array.js
@@ -0,0 +1,8 @@
+// Description:
+// Given an array of integers your solution should find the smallest integer.
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args)
+    }
+}
---
+++ Day 22, 8kyu, string repeat.js
@@ -0,0 +1,10 @@
+// Description:
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+function repeatStr (n, s) {
+    let repeatStr = ''
+    for (let i = 1; i <= n; i++) {
+      repeatStr += s
+    }
+    return repeatStr;
+}
