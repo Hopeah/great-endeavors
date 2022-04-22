3084f388b7f6081b61387b5c712ea35ff275c739
Irina <irina@theluky.com>
2022-04-22 18:31:45 -0400

    8kyu, doubling the integer and returning

---
+++ 8kyu/8kyu, double the integer and return.js
@@ -0,0 +1,19 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+//PREP
+
+//P - parameters
+//Given an integer
+
+//R - returns
+//Return double the integer
+
+//E - examples
+//2 -> 4
+
+//P - pseudocode
+
+function doubleInteger(i) {
+    // multiple i by 2 to double it
+    return i*2;
+}
