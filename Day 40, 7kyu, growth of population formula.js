af7b8001d1f9fa76f37014fbffacccf413dbe224
Irina <irina@theluky.com>
2022-05-07 14:21:12 -0400

    Add day 40, 7kyu, growth of population function

---
+++ Day 40, 7kyu, growth of population formula.js
@@ -0,0 +1,23 @@
+// More generally given parameters:
+
+// p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+
+// the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+// aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+function nbYear(p0, percent, aug, p) {
+    let i = 0;
+    while (p0 < p) {
+      p0 = Math.floor(p0 + p0 * (percent / 100) + aug);
+      i += 1
+    }
+    return i
+}
+
+function nbYear(p0, percent, aug, p) {
+    for (let years = 0; p0 < p; years++) {
+      p0 = Math.floor(p0 + p0 * percent / 100 + aug);
+    }
+    return years
+}
