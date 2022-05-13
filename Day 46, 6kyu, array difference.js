14a6410ebafe93885ac2424bb7940ba37fed81b2
Irina <irina@theluky.com>
2022-05-13 14:47:05 -0400

    Add day 46, 6kyu, taking differnce of two arrays

---
+++ Day 46, 6kyu, array difference.js
@@ -0,0 +1,53 @@
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+// PREP
+
+// P - Parameters
+//Two arrays, only numbers?
+
+//R - Returns
+//Should return an array consisting only of values in a that were not found in b
+
+//E - Examples
+// arrayDiff([1, 2], [1]) => [2]
+// arrayDiff([1, 2, 2, 2, 3], [2]) => [1, 3]
+
+//P - PseudoCode
+
+// function arrayDiff(a, b) {
+//     //create new empty array
+//     let arrayUnique = []
+//     //loop through first array to check for first value in b
+//     for (let i = 0; i < a.lenght; i++) {
+//         //nest loop through b if needed
+//         for (let y = 0; i < b.length; y++) {
+//             //remove value if present in both
+//             if (a[i] !== b[y]) {
+//                 arrayUnique.push(a[i])
+//             }
+//         }
+//     }
+//     //return new array
+//     return arrayUnique
+// }
+
+function arrayDiff(a, b) {
+    let newArr = []
+    //loop through b
+    for (let i = 0; i < b.length; i++) {
+        //filter a to not include a value that we are looping through in b
+        if (a.includes(b[i]) === 'true') {
+            newArr = a.filter(number => number !== b[i])
+        }
+    }
+    //filter a to not include a value that we are looping through in b
+    //return new array
+    return newArr
+}
