d29df018d45c087956f4d6c7db81abdb9502e6af
Irina <irina@theluky.com>
2022-06-17 17:37:26 -0400

    6kyu update

--- 6kyu/6kyu, building cubes.js
+++ 6kyu/6kyu, building cubes.js
@@ -4,7 +4,29 @@
 
 // The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
 
-// Examples:
-// findNb(1071225) --> 45
+//PREP
 
+//P - parameters
+//Given an integer, positive?
+
+//R - returns
+//Return the integer that fits the n^3 + (n-1)^3 + ... + 1^3 = m, if no such integer exist - return -1
+
+//E - examples
+// findNb(1071225) --> 45
 // findNb(91716553919377) --> -1
+
+//P - pseudocode
+
+function findNb(m) {
+    //Set up two values, one to count the sum of the cubes, the other one to count the integer n
+    let sum = 0;
+    let n = 0;
+    //Start looping through the numbers, every iteration add 1 to the integer n, add the current n^3 to the sum
+    while (sum < m) {
+        n += 1
+        sum += n**3
+    }
+    //At the end, check whether the sum equals to m, if it does - return the n, if it doesn't - return -1
+    return sum == m ? n : -1
+}
