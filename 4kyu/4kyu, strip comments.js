c0be62343cfe1ebc9199210602447e9bb7d551d6
Irina <irina@theluky.com>
2022-08-02 12:55:08 -0400

    Add 4kyu, strip comments

---
+++ 4kyu/4kyu, strip comments.js
@@ -0,0 +1,52 @@
+// Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+// The code would be called like so:
+
+// var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+// // result should == "apples, pears\ngrapes\nbananas"
+
+//PREP
+
+//P - parameters
+//Given a string that consists of letters + can contain special characters
+
+//R - returns
+//Return a string that doesn't contain anything after a special character + no whitespaces prior to a special character
+
+//E - example
+// apples, pears # and bananas -> // apples, pears
+// grapes -> // grapes
+// bananas !apples -> // bananas
+
+//P - pseudocode
+
+function solution(input, markers) {
+     //Split the given input string into an array with a new line separator
+     let arr = input.split('\n')
+     //Check each market with a for loop
+     for (let i = 0; i < markers.length; i++) {
+         //Per each marker, map the split input array to 1) if a string contains a marker -> sliced string, 2) if it doesn't contain the marker -> replace the trailing whitespaces 
+         arr = arr.map(line => {
+             const index = line.indexOf(markers[i]);
+             if (index !== 1) return line.slice(0, index);
+             else return line
+         })
+     }
+     //Return the joined array (using new line separator again), trim all the trailing whitespaces
+     return arr.join('\n').trimEnd()
+};
+
+function solution(input, markers) {
+    //Split the given input string into an array with a new line separator
+    let arr = input.split('\n')
+    //Check each market with a for loop
+    for (let i = 0; i < markers.length; i++) {
+        //Per each marker, map the split input array to 1) if a string contains a marker -> sliced string, 2) if it doesn't contain the marker -> replace the trailing whitespaces 
+        arr = arr.map(function(line) {
+            const index = line.indexOf(markers[i])
+            if (index !== 1) return line.substring(0, index)
+            else return line.replace(/\s*$/, '')
+        })
+    }
+    //Return the joined array (using new line separator again), trim all the trailing whitespaces
+    return arr.join('\n').trim()
+};
