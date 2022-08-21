c850037ccea059317887895c61f45ad67a6ee483
Irina <irina@theluky.com>
2022-08-21 06:57:59 -0400

    Add 7kyu, make a function that does arithmetic

---
+++ 7kyu/7kyu, make a function that does arithmetic.js
@@ -0,0 +1,41 @@
+// Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+// a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+// The four operators are "add", "subtract", "divide", "multiply".
+// Try to do it without using if statements!
+
+//PREP
+
+//P - parameters
+//Given two positive integers, and 4 strings that are operators called "add", "subtract", "divide", "multiply"
+
+//R - returns
+//Return the result of the operation on the two numbers
+
+//E - examples
+// 5, 2, "add"      --> 7
+// 5, 2, "subtract" --> 3
+// 5, 2, "multiply" --> 10
+// 5, 2, "divide"   --> 2.5
+
+//P - pseudocode
+
+function arithmetic(a, b, operator){
+    //Use switch operator
+    switch(operator) {
+        case 'add':
+          return a + b;
+        case 'subtract':
+          return a - b;
+        case 'multiply':
+          return a * b;
+        case 'divide':
+          return a / b;
+    }
+}
+
+const arithmetic = (a, b, operator) => ({
+    'add'     : a + b,
+    'subtract': a - b,
+    'multiply': a * b,
+    'divide'  : a / b
+}[operator]);
