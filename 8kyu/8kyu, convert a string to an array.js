b602a571f0de9fa64ff425b99059f0bb37b0c6be
Irina <irina@theluky.com>
2022-08-24 09:19:34 -0400

    Add 8kyu, convert a string to an array

---
+++ 8kyu/8kyu, convert a string to an array.js
@@ -0,0 +1,20 @@
+// Write a function to split a string and convert it into an array of words.
+
+//PREP
+
+//P - parameters
+//Given a string
+
+//R - returns
+//Return a string that is split into an array
+
+//E - examples
+// "Robin Singh" ==> ["Robin", "Singh"]
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+//P - pseudocode
+
+function stringToArray(string){
+	//Split the string
+    return string.split(' ')
+}
